f1a8429162bca04ef1b7171f678439973cd40d9b
Brock Hinkson <brockhinkson@gmail.com>
2018-02-28 06:17:41 +0000

    Updated with read functionality for each customer, stock, and cryptocurrency.

--- finances/urls.py
+++ finances/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('customer/<int:customer_id>/', views.customer_detail, name='customer_detail'),
+    path('stock/<int:stock_id>/', views.stock_detail, name='stock_detail'),
+    path('cryptocurrency/<int:crypto_id>/', views.crypto_detail, name='crypto_detail')
 ]
--- finances/views.py
+++ finances/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
+from .models import Customer, Stock, Crypto
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the finances index.")
+def index( request ):
+    customers = Customer.objects.order_by('last_name')
+    context = {
+        'customers': customers,
+    }
+    return render( request, 'finances/index.html', context )
 
+def customer_detail( request, customer_id ):
+    customer = get_object_or_404( Customer, pk=customer_id )
+    return render( request, 'finances/customer_detail.html', { 'customer': customer } )
+
+def stock_detail( request, stock_id ):
+    stock = get_object_or_404( Stock, pk=stock_id )
+    return render( request, 'finances/stock_detail.html', { 'stock': stock } )
+ 
+def crypto_detail( request, crypto_id ):
+    crypto = get_object_or_404( Crypto, pk=crypto_id )
+    return render( request, 'finances/crypto_detail.html', { 'crypto': crypto } )
